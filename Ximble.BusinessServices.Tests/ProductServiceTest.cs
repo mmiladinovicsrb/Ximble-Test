4552d7e05a1334c64c4bdb0ac7ad605dd7c5b0e3
mmiladinovicsrb <mmiladinovicrs@gmail.com>
2019-08-28 14:22:24 +0200

    Added example of unit test - init

---
+++ Ximble.BusinessServices.Tests/ProductServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Ximble.BusinessServices.Interfaces;
+using Ximble.DataModel;
+
+namespace Ximble.BusinessServices.Tests
+{
+    public class ProductServiceTest
+    {
+        private IProductService _productService;
+        private UnitOfWork _unitOfWork;
+        private List<Product> _products;
+        private GenericRepository<Product> _productRepository;
+        private AdventureWorks2017Entities _dbEntities;
+    }
+}
---
+++ Ximble.WebApi.Tests/ProductUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using Ximble.BusinessEntities;
+using Ximble.WebApi.Controllers;
+
+namespace Ximble.WebApi.Tests
+{
+    [TestClass]
+    public class ProductUnitTest
+    {
+        [TestMethod]
+        public void GetProductByName()
+        {
+            var controller = new ProductController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new System.Web.Http.HttpConfiguration();
+
+            var response = controller.GetByName("Adjustable Race");
+            ProductEntity product;
+            Assert.IsTrue(response.TryGetContentValue<ProductEntity>(out product));
+            Assert.AreEqual("AR-5381", product.ProductNumber);
+        }
+    }
+}
